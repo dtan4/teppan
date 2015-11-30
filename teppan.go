c938f14f7e5d2eacb3011b9f3b0b938a8416fe3b
Daisuke Fujita <dtanshi45@gmail.com>
2015-11-30 16:46:28 +0900

    Embed Base64-encoded environment variables

--- teppan.go
+++ teppan.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 	"text/template"
 
-	"github.com/higanworks/envmap"
 	"github.com/joho/godotenv"
 )
 
 const Usage = "usage: teppan [--base64] <template file>"
+
+func envmap(base64Encode bool) map[string]string {
+	items := make(map[string]string)
+
+	for _, env := range os.Environ() {
+		keyValue := strings.SplitN(env, "=", 2)
+
+		if base64Encode {
+			items[keyValue[0]] = base64.StdEncoding.EncodeToString([]byte(keyValue[1]))
+		} else {
+			items[keyValue[0]] = keyValue[1]
+		}
+	}
+
+	return items
+}
 
 func main() {
 	var (
@@ -25,15 +42,12 @@ func main() {
 	flag.BoolVar(&base64Encode, "base64", false, "Embed Base64-encoded variables")
 	flag.Parse()
 
-	fmt.Println(base64Encode)
-
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to load .env!")
 		os.Exit(1)
 	}
 
-	envs := envmap.All()
 	templateFile := os.Args[len(os.Args)-1]
 	tmpl, err := template.ParseFiles(templateFile)
 	if err != nil {
@@ -41,7 +55,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	err = tmpl.Execute(os.Stdout, envs)
+	err = tmpl.Execute(os.Stdout, envmap(base64Encode))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to apply from template file: %s\n", templateFile)
 		os.Exit(1)
